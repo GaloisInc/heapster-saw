bbbcf3294084aa7327bdb4ab773113b664f5f418
Eddy Westbrook <westbrook@galois.com>
2020-06-22 13:22:30 -0700

    updating proofs to no longer use coq-extensible-records; updated xor_swap_rust_proofs.v to work again

--- examples/xor_swap_proofs.v
+++ examples/xor_swap_proofs.v
@@ -4,7 +4,6 @@ From Coq          Require Import String.
 From Coq          Require Import Vectors.Vector.
 From CryptolToCoq Require Import SAWCoreScaffolding.
 From CryptolToCoq Require Import SAWCoreVectorsAsCoqVectors.
-From Records      Require Import Records.
 
 From CryptolToCoq Require Import SAWCorePrelude.
 From CryptolToCoq Require Import CompMExtra.
--- examples/xor_swap_rust_proofs.v
+++ examples/xor_swap_rust_proofs.v
@@ -4,22 +4,37 @@ From Coq          Require Import String.
 From Coq          Require Import Vectors.Vector.
 From CryptolToCoq Require Import SAWCoreScaffolding.
 From CryptolToCoq Require Import SAWCoreVectorsAsCoqVectors.
-From Records      Require Import Records.
 
 From CryptolToCoq Require Import SAWCorePrelude.
+From CryptolToCoq Require Import CompMExtra.
 
 Load "xor_swap_rust.v".
 Import xor_swap_rust.
 
-Definition noErrors {A} (m:CompM A) : Prop := (m None) -> False.
+Definition xor_swap_spec x1 x2 :
+  CompM {_ : unit &
+             ({_ : SAWCorePrelude.bitvector 64 & unit} *
+              ({_ : SAWCorePrelude.bitvector 64 & unit} * unit))%type} :=
+  returnM (existT _ tt (existT _ x2 tt, ((existT _ x1 tt), tt))).
+Arguments xor_swap_spec /.
 
-Lemma noErrors_returnM A (a:A) : noErrors (returnM a).
+(* FIXME: move lemma to SAWCorePrelude...? *)
+Lemma bvXor_twice_r n x y :
+  SAWCorePrelude.bvXor n (SAWCorePrelude.bvXor n x y) y = x.
 Proof.
-  intro. discriminate H.
-Qed.
+  admit.
+Admitted.
 
-Lemma noErrors_xor_swap x1 x2 : noErrors (xor_swap_rust x1 x2).
+(* FIXME: move lemma to SAWCorePrelude...? *)
+Lemma bvXor_twice_l n x y :
+  SAWCorePrelude.bvXor n (SAWCorePrelude.bvXor n y x) y = x.
 Proof.
-  unfold xor_swap_rust; simpl.
-  apply noErrors_returnM.
+  admit.
+Admitted.
+
+Lemma xor_swap_correct : refinesFun xor_swap_rust xor_swap_spec.
+Proof.
+  prove_refinement.
+  rewrite bvXor_twice_r. rewrite bvXor_twice_l.
+  reflexivity.
 Qed.
