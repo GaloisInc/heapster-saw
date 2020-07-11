8f0c099d92469188277ade375cc2a2ad0b6068f0
Eddy Westbrook <westbrook@galois.com>
2020-07-11 07:47:12 -0700

    added a simple for loop example and started trying to verify it

---
+++ examples/loop.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+/* Add two numbers using a while loop that repeatedly increments */
+int64_t add_loop (int64_t x, int64_t y) {
+  int64_t ret = x;
+  for (uint64_t i = y; i > 0; -- i) {
+    ret++;
+  }
+  return ret;
+}
