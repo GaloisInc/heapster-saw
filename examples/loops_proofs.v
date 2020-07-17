90a41d7949bd2dfcad93a139265619c6cc614ab1
Eddy Westbrook <westbrook@galois.com>
2020-07-17 13:05:04 -0700

    added initial (not yet really working) version of loops_proofs.v

---
+++ examples/loops_proofs.v
@@ -0,0 +1,24 @@
+From Coq          Require Import Lists.List.
+From Coq          Require Import String.
+From Coq          Require Import Vectors.Vector.
+From CryptolToCoq Require Import SAWCoreScaffolding.
+From CryptolToCoq Require Import SAWCoreVectorsAsCoqVectors.
+
+From CryptolToCoq Require Import SAWCorePrelude.
+From CryptolToCoq Require Import CompMExtra.
+
+Require Import Examples.loops.
+Import loops.
+
+Import SAWCorePrelude.
+
+Print add_loop__tuple_fun.
+
+Lemma no_errors_add_loop : refinesFun add_loop (fun _ _ => noErrorsSpec).
+Proof.
+  unfold add_loop, add_loop__tuple_fun, noErrorsSpec.
+(*
+  prove_refinement. 
+Qed.
+*)
+Admitted.
