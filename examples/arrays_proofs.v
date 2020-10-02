1ff78abfd3d5fd8bba41788fac228a0def85c3aa
Matthew Yacavone <matthew@yacavone.net>
2020-10-02 12:30:21 -0400

    fix arrays_proofs.v, update arrays.v and linked_list.v

--- examples/arrays_proofs.v
+++ examples/arrays_proofs.v
@@ -68,71 +68,6 @@ Definition zero_array_noErrors : lrtToType zero_array_lrt := fun x _ =>
                                      noErrorsSpec, tt))
             (fun _ => noErrorsSpec)).
 
-(* See no_errors_zero_array below, eventually we should get rid of this manual version *)
-Lemma no_errors_zero_array_manual : refinesFun zero_array zero_array_noErrors.
-Proof.
-  unfold zero_array, zero_array__tuple_fun, zero_array_noErrors, noErrorsSpec.
-  unfold zero_array_lrt, zero_array_letRec_lrt.
-  unfold bvultWithProof.
-  (* fold bvMem_lo and bvMem_hi for convenience (Why must I hold Coq's hand so much?? Aghhh!!) *)
-  unfold true, false; fold bvMem_lo; fold bvMem_hi.
-  (* unfold the top-level multiFixM on the left *)
-  apply refinesFun_multiFixM_fst.
-  unfold Datatypes.fst, refinesFun; intros.
-  (* refinesM_assumingM_r, and manipulation of the resulting hyp. *)
-  apply refinesM_assumingM_r; intro e_assuming1.
-  destruct e_assuming1 as [ e_assuming1_H1 e_assuming1_H2 ].
-  (* refinesM_letRecM1_fst *)
-  apply refinesM_letRecM1_fst; try apply ProperFun_any.
-  (* 1. proving refinement of the letRec'd functions *)
-  - (* unfold the multiFixM on the left *)
-    apply refinesFun_multiFixM_fst.
-    unfold Datatypes.fst, refinesFun; intros.
-    (* can we get Coq to do this auomatically too? *)
-    rewrite if_bv_lemma.
-    (* refinesM_assumingM_r, and manipulation of the resulting hyp. *)
-    apply refinesM_assumingM_r; intro e_assuming2.
-    destruct e_assuming2 as [ e_assuming2_H1 e_assuming2 ].
-    destruct e_assuming2 as [ e_assuming2_H2 e_assuming2_H3 ].
-    (* refinesM_if_l, and manipulation of the resulting hyp. *)
-    apply refinesM_if_l; intro e_if1; try rewrite e_if1.
-    + apply refinesM_if_l; intro e_if2.
-      * apply refinesM_maybe_l; [ intro e_maybe | intros e_maybe_arg e_maybe ].
-        -- discriminate e_maybe.
-        -- apply refinesM_assumingM_l.
-           (* I. proving the loop invariant *)
-           ++ split; try split; cbn [ projT1 ].
-              ** transitivity (projT1 a3).
-                 --- assumption.
-                 --- apply isBvsle_suc_r.
-                     rewrite e_assuming2_H2, e_assuming1_H2.
-                     reflexivity.
-              ** apply isBvslt_to_isBvsle_suc.
-                 rewrite isBvult_def in e_if1.
-                 rewrite <- e_assuming2_H3 in e_if1.
-                 apply isBvult_to_isBvslt_pos; assumption.
-              ** assumption.
-           (* II. proving refinement of the bodies *)
-           ++ reflexivity.
-      * (* this case is impossible *)
-        rewrite and_bool_eq_false_lemma in e_if2.
-        destruct e_if2 as [ e_if2 | e_if2 ]; rewrite isBvslt_def_opp in e_if2.
-        -- rewrite <- e_assuming2_H1 in e_if2.
-           vm_compute in e_if2; inversion e_if2.
-        -- rewrite e_assuming2_H2, e_assuming1_H2 in e_if2.
-           apply isBvslt_antirefl in e_if2; inversion e_if2.
-    + prove_refinement.
-  (* 2. proving refinement of the letRec bodies *)
-  - apply refinesM_assumingM_l.
-    (* I. proving the initial loop invariant *)
-    + cbn [ projT1 ].
-      split; split.
-      * assumption.
-      * reflexivity.
-    (* II. proving refinement of the bodies *)
-    + reflexivity.
-Qed.
-
 Lemma no_errors_zero_array : refinesFun zero_array zero_array_noErrors.
 Proof.
   unfold zero_array, zero_array__tuple_fun, zero_array_noErrors, noErrorsSpec.
@@ -149,6 +84,7 @@ Proof.
   (* Maybe one day Heapster will be smart enough to know that this case is
      impossible, but for now it's easy enough prove: *)
   - rewrite if_bv_lemma in e_if.
+    cbn [ projT1 ] in e_maybe.
     rewrite e_if in e_maybe.
     discriminate e_maybe.
   (* Proving the loop invariant holds inductively: *)
@@ -219,6 +155,7 @@ Proof.
   all: cbn [ projT1 ].
   - repeat (split; try assumption). (* <- different from no_errors_zero_array *)
   - rewrite if_bv_lemma in e_if.
+    cbn [ projT1 ] in e_maybe.
     rewrite e_if in e_maybe.
     discriminate e_maybe.
   - repeat split.
