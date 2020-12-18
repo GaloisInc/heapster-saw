ae19efbd8d131dd2094f725f1c50a1001df6736e
Eddy Westbrook <westbrook@galois.com>
2020-12-18 10:14:03 -0800

    added new memcpy example file

---
+++ examples/memcpy_proofs.v
@@ -0,0 +1,16 @@
+From Coq          Require Import Lists.List.
+From Coq          Require Import String.
+From Coq          Require Import Vectors.Vector.
+From CryptolToCoq Require Import SAWCoreScaffolding.
+From CryptolToCoq Require Import SAWCoreVectorsAsCoqVectors.
+From CryptolToCoq Require Import SAWCoreBitvectors.
+
+From CryptolToCoq Require Import SAWCorePrelude.
+From CryptolToCoq Require Import CompMExtra.
+
+Require Import Examples.memcpy.
+Import memcpy.
+
+Import SAWCorePrelude.
+
+Print memcpy.
