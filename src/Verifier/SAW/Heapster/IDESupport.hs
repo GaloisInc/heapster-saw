8fe64169cb300bc396c1195e07b17415e6c47d76
Karl Smeltzer <karl@galois.com>
2021-05-24 18:10:42 -0700

    get basic location info working

--- src/Verifier/SAW/Heapster/IDESupport.hs
+++ src/Verifier/SAW/Heapster/IDESupport.hs
@@ -9,81 +9,77 @@
 
 module Verifier.SAW.Heapster.IDESupport where
 
-import Data.Aeson
-import Data.Binding.Hobbits (Mb(..), Liftable, NuMatching, mbLift, mkNuMatching, mbMatch, nuMP)
+import Data.Aeson ( encodeFile, ToJSON )
+import Data.Binding.Hobbits
+    ( nuMP,
+      mbMatch,
+      unsafeMbTypeRepr,
+      Liftable(..),
+      Mb,
+      NuMatching(..) )
 import Data.Parameterized.Some (Some (..))
+import qualified Data.Text as T
 import qualified Data.Type.RList as RL
-import GHC.Generics
-import Lang.Crucible.LLVM.MemModel (GlobalSymbol (..))
-import System.IO (hPutStr, withFile, IOMode(..))
-import Text.LLVM.AST (Symbol (..))
+import GHC.Generics ( Generic )
+import What4.FunctionName ( FunctionName(functionName) )
+import What4.ProgramLoc
+    ( Position(InternalPos, SourcePos, BinaryPos, OtherPos),
+      ProgramLoc(..) )
 
-
+import Verifier.SAW.Heapster.Implication
 import Verifier.SAW.Heapster.Permissions
 import Verifier.SAW.Heapster.TypedCrucible
 
-data LogFileFormat = LogFileFormat 
-  { filename :: String
-  , directory :: String
-  } deriving (Generic, Show)
 
-instance ToJSON LogFileFormat
-instance NuMatching LogFileFormat
-instance Liftable LogFileFormat where
-  mbLift mb = case mbMatch mb of
-    [nuMP| LogFileFormat x y |] -> LogFileFormat (mbLift x) (mbLift y)
-
-data LogPermissionsFormat = LogPermissionsFormat
-  { line :: Int
-  , permission :: String
-  } deriving (Generic, Show)
-
-instance ToJSON LogPermissionsFormat
-instance NuMatching LogPermissionsFormat
-instance Liftable LogPermissionsFormat where
-  mbLift mb = case mbMatch mb of
-    [nuMP| LogPermissionsFormat x y |] -> LogPermissionsFormat (mbLift x) (mbLift y)
-
-
-data LogEntryFormat = LogEntryFormat 
-  { name :: String
-  , file :: LogFileFormat
-  , permissions :: [LogPermissionsFormat]
+-- | A single entry in the IDE info dump log.  At a bare minimum, this must
+-- include a location and corresponding permission.  Once the basics are
+-- working, we can enrich the information we log.
+data LogEntryFormat = LogEntryFormat
+  { lefLocation :: String
+  , lefPermissions :: String
   } deriving (Generic, Show)
 
 instance ToJSON LogEntryFormat
-instance NuMatching LogEntryFormat
+instance NuMatching LogEntryFormat where
+  nuMatchingProof = unsafeMbTypeRepr
 instance Liftable LogEntryFormat where
   mbLift mb = case mbMatch mb of
-    [nuMP| LogEntryFormat x y z |] -> LogEntryFormat (mbLift x) (mbLift y) (mbLift z)
+    [nuMP| LogEntryFormat x y |] -> LogEntryFormat (mbLift x) (mbLift y)
 
+-- | A complete IDE info dump log, which is just a sequence of entries.  Once
+-- the basics are working, we can enrich the information we log.
 data LogMainFormat = LogMainFormat {
-  function :: [LogEntryFormat]
+  lmfEntries :: [LogEntryFormat]
 } deriving (Generic, Show)
 
 instance ToJSON LogMainFormat
 
 
-printIDEInfo :: String -> PermEnv -> [Some SomeTypedCFG] -> IO ()
-printIDEInfo fnName henv tcfgs = do
-  mapM_ handleSomeCFG tcfgs
+-- | The entry point for dumping a Heapster environment to a file for IDE
+-- consumption.
+printIDEInfo :: PermEnv -> [Some SomeTypedCFG] -> FilePath -> IO ()
+printIDEInfo _penv tcfgs file = do
+  let jsonContent = LogMainFormat $ concatMap handleSomeCFG tcfgs
+  encodeFile file jsonContent
 
-handleSomeCFG :: Some SomeTypedCFG -> IO ()
+-- | This just unwraps the `Some` constructors on a CFG, and then calls out to
+-- generate the log entries for it.
+handleSomeCFG :: Some SomeTypedCFG -> [LogEntryFormat]
 handleSomeCFG cfg =
   case cfg of
     Some (SomeTypedCFG tcfg) ->
       handleBlockMap $ tpcfgBlockMap tcfg
 
-
-handleBlockMap :: PermCheckExtC ext => TypedBlockMap ext blocks tops ret -> IO ()
+-- | Each TCFG, which is for a function, will have several basic blocks.  This
+-- is a map containing those blocks, so we handle each seperately and
+-- concatenate the results into one list.
+handleBlockMap :: PermCheckExtC ext => TypedBlockMap ext blocks tops ret -> [LogEntryFormat]
 handleBlockMap bm = do
-  let json = LogMainFormat $ concat $ RL.mapToList handleBlock bm
-  encodeFile "karl" json
-  
+  concat $ RL.mapToList handleBlock bm
+
+-- | For a single basic block, look at each entry point and dump corresponding information.
 handleBlock :: PermCheckExtC ext => TypedBlock ext blocks tops ret args -> [LogEntryFormat]
-handleBlock tb = 
-  concatMap handleTypedEntry' (typedBlockEntries tb)
-  -- concatMap handleTypedEntry' (typedBlockEntries tb) <> ", " <> str
+handleBlock tb = concatMap handleTypedEntry' (typedBlockEntries tb)
 
 -- handleTypedEntry :: PermCheckExtC ext => TypedEntry ext blocks tops ret args -> String
 -- handleTypedEntry (TypedEntry _ _ _ _ _ _ _ perms) =
@@ -92,59 +88,52 @@ handleBlock tb =
 --     [nuMP| TypedConsStmt loc stmt rets seq |] -> ""
 --     [nuMP| TypedTermStmt loc stmt |] -> ""
 
+-- | A single entry point for a particular basic block holds the permissions we
+-- want as well as the location information.  For now, since we're only looking
+-- at entry permissions, we just take the first location in  the statement
+-- sequence and ignore the rest.
 handleTypedEntry' :: PermCheckExtC ext => TypedEntry ext blocks tops ret args -> [LogEntryFormat]
-handleTypedEntry' (TypedEntry _ _ _ _ _ perms _ _) = 
-  handlePerms perms
+handleTypedEntry' (TypedEntry _ _ _ _ _ entryperms _ stmts) =
+  let loc = mbLift $ fmap getFirstProgramLoc stmts
+  in handleMbPerms entryperms loc
 
-handlePerms :: Mb ctx (ValuePerms ctx) -> [LogEntryFormat]
-handlePerms mbvp = mbLift $ fmap handlePerms mbvp
+-- | From the sequence, get the first program location we encounter, which
+-- should correspond to the permissions for the entry point we want to log
+getFirstProgramLoc :: PermCheckExtC ext => TypedStmtSeq ext blocks tops ret ctx -> ProgramLoc
+getFirstProgramLoc (TypedImplStmt (AnnotPermImpl _ pis)) = getFirstProgramLocPI pis
+getFirstProgramLoc (TypedConsStmt loc _ _ _) = loc
+getFirstProgramLoc (TypedTermStmt loc _) = loc
+
+getFirstProgramLocPI :: PermCheckExtC ext => PermImpl (TypedStmtSeq ext blocks tops ret) ctx -> ProgramLoc
+getFirstProgramLocPI (PermImpl_Done stmts) = getFirstProgramLoc stmts
+getFirstProgramLocPI (PermImpl_Step _ mbps) = getFirstProgramLocMBPI mbps
+
+getFirstProgramLocMBPI :: PermCheckExtC ext => MbPermImpls (TypedStmtSeq ext blocks tops ret) ctx -> ProgramLoc
+getFirstProgramLocMBPI MbPermImpls_Nil = error "Error finding program location for IDE log"
+getFirstProgramLocMBPI (MbPermImpls_Cons _ _ pis) = mbLift $ fmap getFirstProgramLocPI pis
+
+-- | Pretty print the permissions and construct a log entry with the given location.
+-- TODO: pattern match on this isntead of mblift
+handleMbPerms :: Mb ctx (ValuePerms ctx) -> ProgramLoc -> [LogEntryFormat]
+handleMbPerms mbvp loc = mbLift $ fmap handlePerms mbvp
   where
     handlePerms :: ValuePerms ctx -> [LogEntryFormat]
     handlePerms perms = foldValuePerms handlePerm [] perms
 
     handlePerm :: [LogEntryFormat] -> ValuePerm ctx -> [LogEntryFormat]
-    handlePerm rest perm = 
-      let str = permPrettyString emptyPPInfo perm
-      in LogEntryFormat "name" (LogFileFormat "filename" "dir") [LogPermissionsFormat 1 str] : rest
--- handlePerm rest (ValPerm_Eq pe) =  "valperm_eq " <> rest
--- handlePerm rest (ValPerm_Or _ _) = "valperm_or " <> rest
--- handlePerm rest (ValPerm_Exists _) = "valperm_exists " <> rest
--- handlePerm rest (ValPerm_Named _ _ _) = "valperm_named " <> rest
--- handlePerm rest (ValPerm_Var _ _) = "valperm_var " <> rest
--- handlePerm rest (ValPerm_Conj _) = "valperm_conj " <> rest
+    handlePerm rest perm =
+      let permStr = permPrettyString emptyPPInfo perm
+          (_, locStr) = ppLoc loc
+      in LogEntryFormat locStr permStr : rest
 
-handlePermExpr :: PermExpr a -> String
-handlePermExpr = permPrettyString emptyPPInfo
+-- | Print a `ProgramLoc` in a way that is useful for an IDE, i.e., machine readable
+ppLoc :: ProgramLoc -> (String, String)
+ppLoc pl =
+  let fnName = T.unpack $ functionName $ plFunction pl
+      locStr = ppPos $ plSourceLoc pl
 
--- data TypedStmtSeq ext blocks tops ret ps_in where
---   -- | A permission implication step, which modifies the current permission
---   -- set. This can include pattern-matches and/or assertion failures.
---   TypedImplStmt :: !(AnnotPermImpl (TypedStmtSeq ext blocks tops ret) ps_in) ->
---                    TypedStmtSeq ext blocks tops ret ps_in
-
---   -- | Typed version of 'ConsStmt', which binds new variables for the return
---   -- value(s) of each statement
---   TypedConsStmt :: !ProgramLoc ->
---                    !(TypedStmt ext rets ps_in ps_next) ->
---                    !(RAssign Proxy rets) ->
---                    !(Mb rets (TypedStmtSeq ext blocks tops ret ps_next)) ->
---                    TypedStmtSeq ext blocks tops ret ps_in
-
---   -- | Typed version of 'TermStmt', which terminates the current block
---   TypedTermStmt :: !ProgramLoc ->
---                    !(TypedTermStmt blocks tops ret ps_in) ->
---                    TypedStmtSeq ext blocks tops ret ps_in
-
--- dumpPerms :: Mb 
-
-
-  -- data TypedEntry ext blocks tops ret args where
-  -- TypedEntry ::
-  --   !(TypedEntryID blocks args ghosts) ->
-  --   !(TypedEntryInDegree) ->
-  --   !(CruCtx tops) -> !(CruCtx args) -> !(TypeRepr ret) ->
-  --   !(MbValuePerms ((tops :++: args) :++: ghosts)) ->
-  --   !(Mb ((tops :++: args) :++: ghosts :> ret) (ValuePerms (tops :> ret))) ->
-  --   !(Mb ((tops :++: args) :++: ghosts)
-  --     (TypedStmtSeq ext blocks tops ret ((tops :++: args) :++: ghosts))) ->
-  --   TypedEntry ext blocks tops ret args
+      ppPos (SourcePos file line column) = T.unpack file <> ":" <> show line <> ":" <> show column
+      ppPos (BinaryPos _ _) = "<unknown binary pos>"
+      ppPos (OtherPos _) = "<unknown other pos>"
+      ppPos InternalPos = "<unknown internal pos>"
+  in (fnName, locStr)
